790bf26ff69f916e6c808cbd4dafa8da4fa1685f
lucasmma <lukito020@gmail.com>
2022-05-25 18:17:07 -0300

    feat: create Sale Model

---
+++ src/domain/models/sale.js
@@ -0,0 +1,11 @@
+const { Joi } = require('celebrate');
+
+module.exports = Joi.object({
+  circleId: Joi.string().required(),
+  saleFrom: Joi.string().required(),
+  saleTo: Joi.string().required(),
+  description: Joi.string().required(),
+  name: Joi.string().required(),
+  price: Joi.string().required(),
+  id: Joi.string(),
+}).or('saleFrom', 'saleTo');
