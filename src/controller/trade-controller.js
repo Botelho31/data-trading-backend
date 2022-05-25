83f62775ac3304eb5c82ac4d53612a4a1f0c003d
lucasmma <lukito020@gmail.com>
2022-05-25 18:55:38 -0300

    feat: create Trading oportunity

--- src/controller/trade-controller.js
+++ src/controller/trade-controller.js
@@ -7,21 +7,28 @@ const tableName = 'user';
 module.exports = {
   async createTrade(req, res, next) {
     try {
-      const { publicAddress } = req.body;
-      const user = await dynamoHelper.queryTableWhereId(tableName, 'publicAddress', publicAddress);
-      if (user !== undefined) {
-        return res.status(409).json({ message: 'PUBLIC_ADDRESS_ALREADY_EXISTS' });
+      if(req.body.id === null){
+        const trade = await dynamoHelper.create(tableName, req.body);
+        console.log(trade);
+        return res.json(trade);
+      } else if(req.body.saleTo !== null) {
+        const trade = await dynamoHelper.updateObject(tableName, {
+          id: req.body.id
+        }, 'saleTo', req.body.saleTo);
+        return res.json(trade);
+      } else {
+        const trade = await dynamoHelper.updateObject(tableName, {
+          id: req.body.id
+        }, 'saleFrom', req.body.saleFrom);
+        return res.json(trade);
       }
-      const nonce = await module.exports.createNonce(publicAddress);
-      req.body.nonce = nonce;
-      return res.json(req.body);
     } catch (error) {
       return next(error);
     }
   },
   async getAll(req, res, next) {
     try {
-      const { publicAddress } = req.body;
+      const { circleId } = req.body;
       const user = await dynamoHelper.queryTableWhereId(tableName, 'publicAddress', publicAddress);
       if (user !== undefined) {
         return res.status(409).json({ message: 'PUBLIC_ADDRESS_ALREADY_EXISTS' });
