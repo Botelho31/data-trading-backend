afb8aa86f752759f95c808cccbf553f504101d00
lucasmma <lukito020@gmail.com>
2022-05-25 10:15:46 -0300

    feat: update files

--- src/domain/models/user.js
+++ src/domain/models/user.js
@@ -2,5 +2,7 @@ const { Joi } = require('celebrate');
 
 module.exports = Joi.object({
   publicAddress: Joi.string().required(),
+  name: Joi.string().required(),
+  email: Joi.string().required(),
   signedNonce: Joi.string(),
 });
