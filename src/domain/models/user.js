00837f62f89493b93e047e3526b5b10ce51a3339
lucasmma <lukito020@gmail.com>
2022-05-26 10:33:28 -0300

    feat: add circleIds to user

--- src/domain/models/user.js
+++ src/domain/models/user.js
@@ -4,5 +4,6 @@ module.exports = Joi.object({
   publicAddress: Joi.string().required(),
   name: Joi.string().required(),
   email: Joi.string().required(),
+  circleIds: Joi.array().string(),
   signedNonce: Joi.string(),
 });
