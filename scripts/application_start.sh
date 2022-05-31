54e269f971404953db885cd5f95bbcef2c861180
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-05-30 23:06:44 -0300

    build: server location is on final folder

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-pm2 delete data-trading 2> /dev/null || : && pm2 start --name "data-trading" ./src/server.js
+pm2 delete data-trading 2> /dev/null || : && pm2 start --name "data-trading" /home/ubuntu/data-trading-backend/src/server.js
