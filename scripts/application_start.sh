7203d3553ced0a4c1fde3f2d0699eb419433da59
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-05-30 23:02:08 -0300

    build: change npm start to node

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-pm2 delete data-trading 2> /dev/null || : && pm2 start --name "data-trading" npm -- start
+pm2 delete data-trading 2> /dev/null || : && pm2 start --name "data-trading" ./src/server.js
