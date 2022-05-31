4d9398118a07144dab9c80177609f05a1ac931f1
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-05-30 22:12:31 -0300

    build: add pm2 full path

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env sh
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-pm2 delete data-trading
+/home/ubuntu/.nvm/versions/node/v18.2.0/bin/pm2 delete data-trading
