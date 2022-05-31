2e9756b5879c0114b9b91c3d9bb7127e9e1aec4d
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-05-30 22:20:40 -0300

    build: changed to run as root

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-/home/ubuntu/.nvm/versions/node/v18.2.0/bin/pm2 start --name "data-trading" npm -- start
+pm2 start --name "data-trading" npm -- start
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-/home/ubuntu/.nvm/versions/node/v18.2.0/bin/pm2 delete data-trading
+pm2 delete data-trading
